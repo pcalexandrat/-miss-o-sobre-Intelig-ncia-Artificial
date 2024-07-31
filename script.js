5e41af9571b39bfc6d5d99650a759672315c4565
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2024-07-31 19:16:08 -0300

    .

--- script.js
+++ script.js
@@ -37,6 +37,7 @@ let perguntaAtual;
 function mostraPergunta(){
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
 
