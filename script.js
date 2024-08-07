d3b76faad9c8874a65e11ea68ff7fe2148f57e6b
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2024-08-07 19:11:29 -0300

    atual++

--- script.js
+++ script.js
@@ -66,6 +66,10 @@ function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click",function(){
+        atual++;
+        mostraPergunta();
+        });
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
